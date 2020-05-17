6c0ebc4c69c8cdbf765c1762da17c81e5ee6a4e3
Chaim Bryski <luzybry94@gmail.com>
2020-05-17 20:29:36 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
-def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
+def reverse_each_word(str)
+  reversed_array = []
+  str_array = str.split(" ")
+  str_array.collect do |word|
+    reversed_array << word.reverse
+  end
+  new_str = reversed_array.join(" ")
+  new_str
 end
