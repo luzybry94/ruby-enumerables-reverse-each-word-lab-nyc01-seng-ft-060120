0bf51715b2835b9048489f1d64a2e07804f80862
Chaim Bryski <luzybry94@gmail.com>
2020-05-27 16:26:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,10 @@
 require "pry"
 
 def reverse_each_word(str)
-  reversed_array = []
   str_array = str.split(" ")
-  str_array.each do |word|
-    binding.pry
-    reversed_array << word.reverse
-    binding.pry
+  str_array.collect do |word|
+    word.reverse
   end
   reversed_string = reversed_array.join(" ")
-  binding.pry
   return reversed_string
 end
