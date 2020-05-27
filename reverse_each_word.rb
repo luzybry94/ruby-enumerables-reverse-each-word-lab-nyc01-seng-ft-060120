2eb7696edb5ee7f68b4a300bd51a8396c38709f8
Chaim Bryski <luzybry94@gmail.com>
2020-05-27 16:20:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 require "pry"
 
 def reverse_each_word(str)
+  reversed_array = []
   str_array = str.split(" ")
-  str_array.collect do |word|
-    word.reverse!
+  str_array.each do |word|
+    reversed_array << word.reverse
   end
   reversed_string = str_array.join(" ")
   return reversed_string
